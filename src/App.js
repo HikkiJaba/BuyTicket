d6f16b5231069e830386a2988017fd6770236b87
Егор Брайт <brait.2005@mail.ru>
2024-04-15 08:50:36 +0700

    add logical filterTransfer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css"
 import React, { useEffect, useState } from 'react';
 import Header from './componens/header';
 import FilterTransfer from './componens/filterTransfer';
@@ -8,47 +8,73 @@ import Card from './componens/Card';
 
 function App() {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true); // Добавляем состояние для отслеживания загрузки
+  const [loading, setLoading] = useState(true);
+  const [selectedFilters, setSelectedFilters] = useState({
+    all: true,
+    noTransfers: false,
+    oneTransfer: false,
+    twoTransfers: false,
+    threeTransfers: false,
+  });
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('/api/v2/prices/latest?currency=rub&period_type=year&page=1&limit=30&show_to_affiliates=true&sorting=price&trip_class=0&token=1662ebbbc7639b5c5efa22a72cbe5849');
         setData(response.data.data);
-        setLoading(false); // Устанавливаем состояние загрузки в false после получения данных
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching data:', error);
-        setLoading(false); // В случае ошибки также устанавливаем состояние загрузки в false
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
-  console.log(data);
+  const applyFilters = () => {
+    let filteredData = data;
+    if (!selectedFilters.all) {
+      filteredData = filteredData.filter(item => {
+        if (selectedFilters.noTransfers && item.number_of_changes === 0) return true;
+        if (selectedFilters.oneTransfer && item.number_of_changes === 1) return true;
+        if (selectedFilters.twoTransfers && item.number_of_changes === 2) return true;
+        if (selectedFilters.threeTransfers && item.number_of_changes === 3) return true;
+        return false;
+      });
+    }
+    return filteredData;
+  };
+
+  const handleFilterChange = (filter) => {
+    setSelectedFilters(prevState => ({
+      ...prevState,
+      [filter]: !prevState[filter],
+    }));
+  };
 
   return (
     <div className="App">
       <Header />
       <div className='filterMain'>
-        <FilterTransfer />
+        <FilterTransfer handleFilterChange={handleFilterChange} selectedFilters={selectedFilters} />
         <div>
-          <FilterCheapFast /> 
-          {loading ? ( // Показываем индикатор загрузки, если данные загружаются
+          <FilterCheapFast />
+          {loading ? (
             <div className='loading'>Loading...</div>
           ) : (
             <div className='card-container'>
-              {data && data.map((item, index) => (
+              {applyFilters().map((item, index) => (
                 <Card key={index} 
-                origin={item.origin}
-                destination={item.destination} 
-                distance={item.distance} 
-                depart_date={item.depart_date} 
-                return_date={item.return_date} 
-                value={item.value} 
-                gate={item.gate} 
-                number_of_changes={item.number_of_changes} 
-                trip_class={item.trip_class}/>
+                  origin={item.origin}
+                  destination={item.destination} 
+                  distance={item.distance} 
+                  depart_date={item.depart_date} 
+                  return_date={item.return_date} 
+                  value={item.value} 
+                  gate={item.gate} 
+                  number_of_changes={item.number_of_changes} 
+                  trip_class={item.trip_class}/>
               ))}
             </div>
           )}
--- src/componens/filterTransfer.js
+++ src/componens/filterTransfer.js
@@ -1,36 +1,61 @@
 import React from "react";
 
-export default function FilterTransfer() {
- return(
+export default function FilterTransfer({ handleFilterChange, selectedFilters }) {
+  return (
     <div>
       <div className="blockfilterTransfer">
-         <div><p>Количество пересадок:</p></div>
+        <div><p>Количество пересадок:</p></div>
         <label className="numTransfer">
-            <input type="checkbox" name="coding-notes" className="real-checkbox" />
-            <span className="custom-checkbox"></span>
-            Все
+          <input 
+            type="checkbox" 
+            name="all" 
+            className="real-checkbox" 
+            checked={selectedFilters.all} 
+            onChange={() => handleFilterChange('all')} />
+          <span className="custom-checkbox"></span>
+          Все
         </label>
         <label className="numTransfer">
-            <input type="checkbox" name="coding-notes" className="real-checkbox" />
-            <span className="custom-checkbox"></span>
-            Без пересадок
+          <input 
+            type="checkbox" 
+            name="noTransfers" 
+            className="real-checkbox" 
+            checked={selectedFilters.noTransfers} 
+            onChange={() => handleFilterChange('noTransfers')} />
+          <span className="custom-checkbox"></span>
+          Без пересадок
         </label>
         <label className="numTransfer">
-            <input type="checkbox" name="coding-notes" className="real-checkbox" />
-            <span className="custom-checkbox"></span>
-            1 пересадка
+          <input 
+            type="checkbox" 
+            name="oneTransfer" 
+            className="real-checkbox" 
+            checked={selectedFilters.oneTransfer} 
+            onChange={() => handleFilterChange('oneTransfer')} />
+          <span className="custom-checkbox"></span>
+          1 пересадка
         </label>
         <label className="numTransfer">
-            <input type="checkbox" name="coding-notes" className="real-checkbox" />
-            <span className="custom-checkbox"></span>
-            2 пересадки
+          <input 
+            type="checkbox" 
+            name="twoTransfers" 
+            className="real-checkbox" 
+            checked={selectedFilters.twoTransfers} 
+            onChange={() => handleFilterChange('twoTransfers')} />
+          <span className="custom-checkbox"></span>
+          2 пересадки
         </label>
         <label className="numTransfer">
-            <input type="checkbox" name="coding-notes" className="real-checkbox" />
-            <span className="custom-checkbox"></span>
-            3 пересадки
+          <input 
+            type="checkbox" 
+            name="threeTransfers" 
+            className="real-checkbox" 
+            checked={selectedFilters.threeTransfers} 
+            onChange={() => handleFilterChange('threeTransfers')} />
+          <span className="custom-checkbox"></span>
+          3 пересадки
         </label>
       </div>
     </div>
- )
+  );
 }
